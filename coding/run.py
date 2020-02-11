eb1d780d67d59142331d93d07368c96220f15d0c
Abhijit Roy <dalvath3700@gmail.com>
2020-02-11 09:24:45 +0530

    coding modified to check the no. of files with .001 ext. & README pdf created

--- coding/run.py
+++ coding/run.py
@@ -26,50 +26,50 @@ def replace_line(file_name, line_num_lotid, text_lotid, line_num_resultsid, text
     out.close()
 
 # ###############################################################################################
-for root, dirs, files in os.walk("./"):
-    for file in files:
-        if file.endswith(file_extension):
-            # print(file)     # print the file name with extension
-
-            # ------------------------------------------------------------------------------------
-            """open file & read required parameters"""
-            with open(file, 'r') as fp:
-                lines = fp.read().splitlines()
-                for l in lines:
-                    if lotid in l:
-                        lotid_lineno = lines.index(l)
-                        lotid_val = re.findall('"([^"]*)"', l)[0]       # extract within quotes
-                    if resultsid in l:
-                        resultsid_lineno = lines.index(l)
-                        resultsid_val = re.findall('"([^"]*)"', l)[0]       # extract within quotes
-                    if stepid in l:
-                        stepid_lineno = lines.index(l)
-                        stepid_val = re.findall('"([^"]*)"', l)[0]      # extract within quotes
-                    if waferid in l:
-                        waferid_lineno = lines.index(l)
-                        waferid_val = re.findall('"([^"]*)"', l)[0]     # extract within quotes
-
-            # print(lotid_lineno, resultsid_lineno, stepid_lineno, waferid_lineno)      # e.g. --> 5 9 15
-            # print(lotid_val, resultsid_val, stepid_val, waferid_val)       # e.g. --> F19310002.F1 POLY 01
-
-            # ------------------------------------------------------------------------------------
-            """Replace modified text in respective params"""
-            replace_line(
-                file_name= file,
-                line_num_lotid= lotid_lineno,
-                text_lotid= lotid + ' "' + lotid_val + stepid_val + '"\n',
-                line_num_resultsid= resultsid_lineno,
-                text_resultsid= resultsid + ' "' + resultsid_val + stepid_val + '"\n',
-                )
-
-
-            # ------------------------------------------------------------------------------------
-            """ Change the Filename"""
-            output_filename = lotid_val + '_' + stepid_val + '_WF' + waferid_val + '.001'
-            # print(output_filename)              # e.g. --> F19310002.F1_POLY_WF01
-
-            os.rename(file, output_filename)
-
+for root, dirs, files in os.walk("./"): 
     """List comprehension for printing the message if there are no files with .001 file extension"""
-    if len([x for x in files if file_extension in x]) == 0:
+    if len([x for x in files if file_extension in x]) > 0:
+        for file in files:
+            if file.endswith(file_extension):
+                # print(file)     # print the file name with extension
+
+                # ------------------------------------------------------------------------------------
+                """open file & read required parameters"""
+                with open(file, 'r') as fp:
+                    lines = fp.read().splitlines()
+                    for l in lines:
+                        if lotid in l:
+                            lotid_lineno = lines.index(l)
+                            lotid_val = re.findall('"([^"]*)"', l)[0]       # extract within quotes
+                        if resultsid in l:
+                            resultsid_lineno = lines.index(l)
+                            resultsid_val = re.findall('"([^"]*)"', l)[0]       # extract within quotes
+                        if stepid in l:
+                            stepid_lineno = lines.index(l)
+                            stepid_val = re.findall('"([^"]*)"', l)[0]      # extract within quotes
+                        if waferid in l:
+                            waferid_lineno = lines.index(l)
+                            waferid_val = re.findall('"([^"]*)"', l)[0]     # extract within quotes
+
+                # print(lotid_lineno, resultsid_lineno, stepid_lineno, waferid_lineno)      # e.g. --> 5 9 15
+                # print(lotid_val, resultsid_val, stepid_val, waferid_val)       # e.g. --> F19310002.F1 POLY 01
+
+                # ------------------------------------------------------------------------------------
+                """Replace modified text in respective params"""
+                replace_line(
+                    file_name= file,
+                    line_num_lotid= lotid_lineno,
+                    text_lotid= lotid + ' "' + lotid_val + stepid_val + '"\n',
+                    line_num_resultsid= resultsid_lineno,
+                    text_resultsid= resultsid + ' "' + resultsid_val + stepid_val + '"\n',
+                    )
+
+
+                # ------------------------------------------------------------------------------------
+                """ Change the Filename"""
+                output_filename = lotid_val + '_' + stepid_val + '_WF' + waferid_val + '.001'
+                # print(output_filename)              # e.g. --> F19310002.F1_POLY_WF01
+
+                os.rename(file, output_filename)
+    else:
         print("SORRY! There are no files found with extension -- .001")
